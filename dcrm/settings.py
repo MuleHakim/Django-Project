c004c8ae1734db04e0286fe5526c33993bdded65
Muluken Hakim <mulukenhakim97@gmail.com>
2023-06-24 11:20:27 +0300

    update

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydatabase',
         'USER': 'root',
-        'PASSWORD': 'mu97@woR',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
